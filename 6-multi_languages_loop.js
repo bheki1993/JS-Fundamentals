ba84a1cba7710537762bbe709a6e2af3ca51af5a
Bhekumuzi Sodinga <Bhekumuzisodinga@gmail.com>
2025-05-22 19:32:45 +0200

    committed multi languages loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+let multi_languages =["C is fun","Python is cool","JavaScript is amazing"];
+let i = 0;
+
+do{
+    console.log(multi_languages[i]);
+         i++;
+}
+ while (i < multi_languages.length);
