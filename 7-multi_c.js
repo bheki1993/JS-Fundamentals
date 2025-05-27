0f68d4465c6c0e37ad07453b12f449795252eb7b
Bhekumuzi Sodinga <Bhekumuzisodinga@gmail.com>
2025-05-27 14:43:42 +0200

    added 7-multi c file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+const args = process.argv;
+const x = parseInt(args[2]);
 
-const x = Number(process.argv[2]);
-
-
- if(!Number.isInteger(x)){
-    console.log("Missing number of occurrences");
-}else{
-    for(let i = 0; i < x; i++){
-        console.log("C is fun");
-    }
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
 
