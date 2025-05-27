ef746575d9d6ce5fa7e2cec9c5392b4a7daf6a1c
Bhekumuzi Sodinga <Bhekumuzisodinga@gmail.com>
2025-05-27 14:49:30 +0200

    fixed 7-multi_c file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,11 +2,8 @@
 const args = process.argv;
 const x = parseInt(args[2]);
 
-if (isNaN(x)) {
-  console.log("Missing number of occurrences");
-} else {
-  for (let i = 0; i < x; i++) {
-    console.log("C is fun");
-  }
+for (let i = 0; i < (isNaN(x) ? 1 : x); i++) {
+  console.log(isNaN(x) ? "Missing number of occurrences" : "C is fun");
 }
+
 
